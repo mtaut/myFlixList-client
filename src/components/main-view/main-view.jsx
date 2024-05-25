dad3b1711e7074df8f21babec3e930338652e349
mtaut <146488331+mtaut@users.noreply.github.com>
2024-05-24 18:33:41 -0600

    setMovies and prop-types update
    Updated main-view and movie-card with setMovies and prop-types

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,42 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Tetris",
-      image:
-        "https://www.imdb.com/title/tt12758060/mediaviewer/rm1597778433/?ref_=ttmi_mi_all_2",
-      director: "Jon Baird",
-      genre: "Drama",
-      description:
-        "Henk Rogers discovers Tetris in 1988 and then risks everything by travelling to the Soviet Union, where he joins forces with inventor Alexey Pajitnov to bring the game to the masses.",
-    },
-    {
-      id: 2,
-      title: "Batman Begins",
-      image:
-        "https://en.wikipedia.org/wiki/Batman_Begins#/media/File:Batman_Begins_Poster.jpg",
-      director: "Christopher Nolan",
-      genre: "Action",
-      description:
-        "After witnessing his parents death, Bruce Wayne learns the art of fighting to confront injustice. When he returns to Gotham as Batman, he must stop a secret society that intends to destroy the city.",
-    },
-    {
-      id: 3,
-      title: "The Departed",
-      image:
-        "https://en.wikipedia.org/wiki/The_Departed#/media/File:Departed234.jpg",
-      director: "Martin Scorsese",
-      genre: "Drama",
-      description:
-        "An undercover cop and a mole in the police attempt to identify each other while infiltrating an Irish gang in South Boston.",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://git.heroku.com/myflixlist.git")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            description: doc.description,
+            genre: doc.genre,
+            director: doc.director,
+            image: imageUrl, // unsure if this is correct...
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
