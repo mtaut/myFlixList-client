1f37454dd9fcbf1ab59ec15fa94d3a50ab4af9f8
mtaut <146488331+mtaut@users.noreply.github.com>
2024-05-30 10:57:51 -0600

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,7 +8,7 @@ export const MainView = () => {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
-    fetch("https://myflixlist-7625107afe99.herokuapp.com/")
+    fetch("https://myflixlist-7625107afe99.herokuapp.com/movies")
       .then((response) => response.json())
       .then((data) => {
         const moviesFromApi = data.docs.map((doc) => {
