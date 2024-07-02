d7fd5da3803fb80aa9838b1a37ae2f78b72632ff
mtaut <146488331+mtaut@users.noreply.github.com>
2024-07-02 15:37:11 -0500

    Signup-view change

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -41,7 +41,7 @@ export const SignupView = () => {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           required
-          minLength="3"
+          minLength="6"
         />
       </label>
       <label>
