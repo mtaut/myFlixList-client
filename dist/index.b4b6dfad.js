5e3b6b695dd6df3f239a4b6b2ac790d417b49959
mtaut <146488331+mtaut@users.noreply.github.com>
2024-07-05 16:51:07 -0500

    signup-view update

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -34410,7 +34410,7 @@ const SignupView = ()=>{
             Email: email,
             Birthday: birthday
         };
-        fetch("https://myflixlist-7625107afe99.herokuapp.com", {
+        fetch("https://myflixlist-7625107afe99.herokuapp.com/users", {
             method: "POST",
             body: JSON.stringify(data),
             headers: {
