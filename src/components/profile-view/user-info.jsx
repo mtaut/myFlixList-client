acffd632d2806ad2fae1d46d7f16324d98ce44d1
mtaut <146488331+mtaut@users.noreply.github.com>
2024-07-18 14:41:26 -0500

    Update user-info.jsx

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export const UserInfo = ({ email, name }) => {
   return (
     <>
-      <h4>{user.Username}'s Profile</h4>
       <p>Name: {name}</p>
       <p>Email: {email}</p>
     </>
   );
+};
+
+UserInfo.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
 };
 
 export default UserInfo;
