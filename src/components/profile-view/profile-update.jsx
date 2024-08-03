57942b5829df14eb99066a3938f5677719162cf1
mtaut <146488331+mtaut@users.noreply.github.com>
2024-08-03 13:42:07 -0500

    Update profile-update.jsx

--- src/components/profile-view/profile-update.jsx
+++ src/components/profile-view/profile-update.jsx
@@ -3,33 +3,47 @@ import PropTypes from "prop-types";
 import { Button, Form, Spinner } from "react-bootstrap";
 import axios from "axios";
 
-export const ProfileUpdate = ({ user, token, onProfileUpdate }) => {
-  const [username, setUsername] = useState(user.Username);
+export const ProfileUpdate = ({ user, token, onProfileUpdate, loading }) => {
+  const [username, setUsername] = useState(user.Username || "");
   const [password, setPassword] = useState("");
-  const [email, setEmail] = useState(user.Email);
-  const [birthday, setBirthday] = useState(user.Birthday);
+  const [email, setEmail] = useState(user.Email || "");
+  const [birthday, setBirthday] = useState(
+    user.Birthday ? user.Birthday.split("T")[0] : ""
+  );
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    if (password && password.length < 8) {
+      alert("Password must be at least 8 characters long");
+      return;
+    }
+
+    const updateData = {
+      Username: username,
+      Email: email,
+      Birthday: birthday,
+    };
+
+    if (password) {
+      updateData.Password = password;
+    }
+
     axios
       .put(
         `https://myflixlist-7625107afe99.herokuapp.com/users/${user.Username}`,
-        {
-          Username: username,
-          Password: password,
-          Email: email,
-          Birthday: birthday,
-        },
+        updateData,
         {
           headers: { Authorization: `Bearer ${token}` },
         }
       )
       .then((response) => {
         onProfileUpdate(response.data);
+        alert("Profile updated successfully");
       })
       .catch((error) => {
-        console.error("Error updating proifle:", error);
+        console.error("Error updating profile:", error);
+        alert("An error occured while updating your profile");
       });
   };
 
@@ -52,7 +66,6 @@ export const ProfileUpdate = ({ user, token, onProfileUpdate }) => {
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          required
           minLength="8"
           placeholder="Password must be 8 or more characters"
         />
@@ -78,7 +91,7 @@ export const ProfileUpdate = ({ user, token, onProfileUpdate }) => {
         />
       </Form.Group>
       <div className="spacer" style={{ margin: "20px" }}></div>
-      <Button type="button" class="btn btn-primary">
+      <Button variant="primary" type="submit" className="update-button">
         {loading ? <Spinner animation="border" size="sm" /> : "Submit"}
       </Button>
     </Form>
@@ -89,6 +102,7 @@ ProfileUpdate.propTypes = {
   user: PropTypes.object.isRequired,
   token: PropTypes.string.isRequired,
   onProfileUpdate: PropTypes.func.isRequired,
+  loading: PropTypes.bool,
 };
 
 export default ProfileUpdate;
